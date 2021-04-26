81a437cb230243bd18ca469c23915dafe4d0d35e
Daniel Son <dson.email@gmail.com>
2021-04-27 09:44:09 +1000

    End of 140. Info from parent to child via props.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import react from 'react';
+import Accordion from './components/Accordion';
 
+const items = [
+    {
+        title: 'What\'s React?',
+        content: 'React is a frontend JavaScript framework.'
+    },
+    {
+        title: 'What are React Hooks?',
+        content: 'Hooks are functions that give functional components the advantages of class components, like lifecycle methods and state.'
+    },
+    {
+        title: 'What are needed in class components, that are\nt needed in functional components?',
+        content: 'Class components need a render function, which returns JSX. Functional components just need to return JSX. Class components must also extend "React.Components".'
+    }
+]
 export default () => {
     return (
-        <div>App!</div>
+        <Accordion items={items} />
     );
 }
 
---
+++ src/components/Accordion.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Accordion = (props) => {
+    return (
+        <div>Acc! {props.items.length} </div>
+    );
+}
+
+export default Accordion;
