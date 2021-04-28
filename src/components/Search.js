08db9dd687683d8e42eda250e3644ccbffa63598
Daniel Son <dson.email@gmail.com>
2021-04-28 21:33:14 +1000

    End of 157. Async/await function inside useEffect() hook.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
     console.log("term: ", term);
 
     useEffect(() => {
-        console.log('useEffect yo');
-    });
-    
+        const searchWiki = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+        }
+        searchWiki();
+    }, [term]);
+
     return (
         <div>
             <div className="ui form">
