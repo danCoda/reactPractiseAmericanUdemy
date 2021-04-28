ea35466c9c67aa2af46d3a09ad4b4e0e00f0c8ec
Daniel Son <dson.email@gmail.com>
2021-04-28 20:14:05 +1000

    End of 146. Accordion working.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,22 +8,26 @@ const Accordion = (props) => {
     }
 
     const renderedItems = props.items.map((item, i) => {
-        return <React.Fragment key={item.title}>
-            <div className="title active" onClick={() => onTitleClick(i)}>
-                <i className="dropdown icon"></i>
-                {item.title}
-            </div>
-            <div className="content active">
-                <p>
-                    {item.content}
-                </p>
-            </div>
-        </ React.Fragment>
+        const active = i === activeIndex ? 'active' : '';
+
+        return (
+            <React.Fragment key={item.title}>
+                <div className={`title ${active}`} onClick={() => onTitleClick(i)}>
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className={`content ${active}`}>
+                    <p>
+                        {item.content}
+                    </p>
+                </div>
+            </ React.Fragment>
+        )
     });
+    
     return (
         <div className="ui styled accordion">
             {renderedItems}
-            <h1>{activeIndex}</h1>
         </div>
     );
 }
