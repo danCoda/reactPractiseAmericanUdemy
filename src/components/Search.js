ee81164773800dc39e2418721d2bc4ab71a9c887
Daniel Son <dson.email@gmail.com>
2021-04-28 20:31:29 +1000

    End of 152. Text input invokes useState hook's setter function.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
+    const [term, setTerm] = useState('');
+    console.log("term: ", term);
     return (
-        <div>Search</div>
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                        className="input" 
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                    />
+                </div>
+            </div>
+        </div>
     );
 };
 
