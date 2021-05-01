e612f24c7a550481e4811c99f5c66317f5d09e18
Daniel Son <dson.email@gmail.com>
2021-05-01 23:10:00 +1000

    End of 193. Google translate API implemented.

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
     {
@@ -35,6 +36,11 @@ const Translate = () => {
                 selected={language}
                 onSelectedChange={setLanguage}
             />
+
+            <hr />
+
+            <h3 className="ui header">Output</h3>
+            <Convert language={language} text={text} />
         </div>
     );
 };
