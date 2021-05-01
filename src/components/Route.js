f7d6662c4b5723b169126387a9635852d6e69af8
Daniel Son <dson.email@gmail.com>
2021-05-02 08:29:35 +1000

    End of 201. Navigation added.

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,7 @@
 // FYI, we aren't using JSX here, so no need to import react.
 
 const Route = ({ path, children }) => {
+    // If the current URL pathname matches the path, show (or 'return') the children.
     return window.location.pathname === path 
         ? children
         : null;
