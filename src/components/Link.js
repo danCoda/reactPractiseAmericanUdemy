a7780895562eea44af0268b5c1b27d067014fdb0
Daniel Son <dson.email@gmail.com>
2021-05-02 09:05:38 +1000

    End of 207. Kinda negligable video; allowing 'Ctrl + click' / natural browser behaviour, to open links in new tabs (as user expects natural browser behaviour).

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Link = ({ className, href, children}) => {
     const onClick = e => {
+        if (e.ctrlKey || e.metaKey) {
+            return; // Let the browser act naturally... Open up a new tab, and thus redownload the page.
+        }
+
         e.preventDefault(); // Prevent full page reload.
         window.history.pushState({}, '', href); // Update URL.
 
+        // Our emitter...
         const navEvent = new PopStateEvent('popstate');
         window.dispatchEvent(navEvent);
     };
