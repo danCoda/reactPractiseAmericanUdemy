8bb9ce5eb4e4d6654026328fc4a94e4a77036bc5
Daniel Son <dson.email@gmail.com>
2021-05-01 23:23:08 +1000

    End of 195. End of the translation. Bandwidth saved by limiting API calls; 500ms is needed after keypress before API call.

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,13 +3,25 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    // "Debounced text" is the text we want to translate... but we request for a translation only if 'text' doesn't change for 500ms; save bandwidth.
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            if (timerId) clearTimeout(timerId);
+        }
+    }, [text]);
 
     useEffect(() => {
-
+        // This helper function's needed because useEffect can't directly do asynchronous code.
         const doTranslation = async() => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 }
@@ -17,8 +29,7 @@ const Convert = ({ language, text }) => {
             setTranslated(data.data.translations[0].translatedText);
         };
         doTranslation();
-        
-    }, [language, text]);
+    }, [language, debouncedText]);
 
     return (
         <div className="ui header">
